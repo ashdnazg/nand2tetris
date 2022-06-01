53bffbee4710b4f7e026f20791f98997c231f780
Eshed <ashdnazg@gmail.com>
2022-06-01 22:34:24 +0300

    gl

--- src/bin/emulator.rs
+++ src/bin/emulator.rs
@@ -1,4 +1,3 @@
-#![forbid(unsafe_code)]
 // #![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] //Hide console window in release builds on Windows, this blocks stdout.
@@ -12,11 +11,146 @@ use eframe::epaint::Vec2;
 
 use egui_extras::{Size, StripBuilder};
 
+use glow::NativeTexture;
 use nand2tetris::hardware::*;
 use nand2tetris::vm::*;
 
+use egui::mutex::Mutex;
+use std::sync::Arc;
+
+struct Screen {
+     program: glow::Program,
+     vertex_array: glow::VertexArray,
+     texture: glow::NativeTexture,
+}
+
+impl Screen {
+    fn new(gl: &glow::Context) -> Self {
+        use glow::HasContext as _;
+
+        let shader_version = if cfg!(target_arch = "wasm32") {
+            "#version 300 es"
+        } else {
+            "#version 410"
+        };
+
+        unsafe {
+            let program = gl.create_program().expect("Cannot create program");
+
+            let (vertex_shader_source, fragment_shader_source) = (
+                r#"
+                    const vec2 verts[3] = vec2[3](
+                        vec2(0.0, 1.0),
+                        vec2(-1.0, -1.0),
+                        vec2(1.0, -1.0)
+                    );
+
+                    out vec2 v_pos;
+                    void main() {
+                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
+                        v_pos = gl_Position.xy;
+                    }
+                "#,
+                r#"
+                    precision mediump float;
+                    uniform sampler2D u_screen;
+                    in vec2 v_pos;
+                    out vec4 out_color;
+                    void main() {
+                        out_color = texture2D(u_screen, (v_pos + 1.0) * 0.5); // + vec4(0.5);
+                    }
+                "#,
+            );
+
+            let shader_sources = [
+                (glow::VERTEX_SHADER, vertex_shader_source),
+                (glow::FRAGMENT_SHADER, fragment_shader_source),
+            ];
+
+            let shaders: Vec<_> = shader_sources
+                .iter()
+                .map(|(shader_type, shader_source)| {
+                    let shader = gl
+                        .create_shader(*shader_type)
+                        .expect("Cannot create shader");
+                    gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
+                    gl.compile_shader(shader);
+                    if !gl.get_shader_compile_status(shader) {
+                        panic!("{}", gl.get_shader_info_log(shader));
+                    }
+                    gl.attach_shader(program, shader);
+                    shader
+                })
+                .collect();
+
+            gl.link_program(program);
+            if !gl.get_program_link_status(program) {
+                panic!("{}", gl.get_program_info_log(program));
+            }
+
+            for shader in shaders {
+                gl.detach_shader(program, shader);
+                gl.delete_shader(shader);
+            }
+
+            let vertex_array = gl
+                .create_vertex_array()
+                .expect("Cannot create vertex array");
+
+            let buffer = vec![127u8;300 * 300 * 4];
+            let texture = gl.create_texture().unwrap();
+            gl.bind_texture(glow::TEXTURE_2D, Some(texture));
+            gl.tex_image_2d(
+                glow::TEXTURE_2D,
+                0,
+                glow::RGBA as i32,
+                300,
+                300,
+                0,
+                glow::RGBA,
+                glow::UNSIGNED_BYTE,
+                Some(&buffer),
+            );
+            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_MIN_FILTER, glow::NEAREST as i32);
+            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_MAG_FILTER, glow::NEAREST as i32);
+            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_WRAP_S, glow::REPEAT as i32);
+            gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_WRAP_T, glow::REPEAT as i32);
+            gl.bind_texture(glow::TEXTURE_2D, None);
+
+
+            Self {
+                program,
+                vertex_array,
+                texture,
+            }
+        }
+    }
+
+    fn destroy(&self, gl: &glow::Context) {
+        use glow::HasContext as _;
+        unsafe {
+            gl.delete_program(self.program);
+            gl.delete_vertex_array(self.vertex_array);
+            gl.delete_texture(self.texture);
+        }
+    }
+
+    fn paint(&self, gl: &glow::Context) {
+        use glow::HasContext as _;
+        unsafe {
+            gl.clear(glow::COLOR);
+            gl.use_program(Some(self.program));
+            gl.active_texture(glow::TEXTURE0);
+            gl.bind_texture(glow::TEXTURE_2D, Some(self.texture));
+            gl.uniform_1_i32(gl.get_uniform_location(self.program, "u_screen").as_ref(), 0);
+            gl.bind_vertex_array(Some(self.vertex_array));
+            gl.draw_arrays(glow::TRIANGLES, 0, 3);
+        }
+    }
+}
+
 struct HardwareState {
-    hardware: Hardware,
+    hardware: Hardware
 }
 
 struct VMState {
@@ -48,14 +182,37 @@ enum Action {
 
 pub struct EmulatorApp {
     state: AppState,
+    screen: Arc<Mutex<Screen>>,
 }
 
-impl Default for EmulatorApp {
-    fn default() -> Self {
+impl EmulatorApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self {
             state: Default::default(),
+            screen: Arc::new(Mutex::new(Screen::new(&cc.gl)))
         }
     }
+}
+
+fn draw_screen(ui: &mut egui::Ui, screen: &Arc<Mutex<Screen>>, ram: &RAM) {
+    let (rect, _) =
+        ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
+
+    // Clone locals so we can move them into the paint callback:
+    let angle = 0.0;
+    let screen = screen.clone();
+
+    let callback = egui::PaintCallback {
+        rect,
+        callback: std::sync::Arc::new(move |_info, render_ctx| {
+            if let Some(painter) = render_ctx.downcast_ref::<egui_glow::Painter>() {
+                screen.lock().paint(painter.gl());
+            } else {
+                eprintln!("Can't do custom painting because we are not using a glow context");
+            }
+        }),
+    };
+    ui.painter().add(callback);
 }
 
 fn draw_shared(ctx: &egui::Context, action: &mut Option<Action>) {
@@ -71,7 +228,7 @@ fn draw_shared(ctx: &egui::Context, action: &mut Option<Action>) {
     });
 }
 
-fn draw_hardware(state: &HardwareState, ctx: &egui::Context, action: &mut Option<Action>) {
+fn draw_hardware(state: &HardwareState, ctx: &egui::Context, action: &mut Option<Action>, screen: &Arc<Mutex<Screen>>) {
     egui::CentralPanel::default().show(ctx, |ui| {
         StripBuilder::new(ui)
             .size(Size::relative(0.5))
@@ -89,6 +246,11 @@ fn draw_hardware(state: &HardwareState, ctx: &egui::Context, action: &mut Option
                                 ui.ram_grid("RAM", &state.hardware.ram, 0..i16::MAX);
                             });
                         });
+                });
+                strip.cell(|ui| {
+                    egui::Frame::canvas(ui.style()).show(ui, |ui| {
+                        draw_screen(ui, screen, &state.hardware.ram);
+                    });
                 });
             });
     });
@@ -243,7 +405,7 @@ impl eframe::App for EmulatorApp {
         draw_shared(ctx, &mut action);
 
         match &self.state {
-            AppState::Hardware(state) => draw_hardware(state, ctx, &mut action),
+            AppState::Hardware(state) => draw_hardware(state, ctx, &mut action, &self.screen),
             AppState::VM(state) => draw_vm(state, ctx, &mut action),
             AppState::Start => draw_start(ctx, &mut action),
         };
@@ -257,6 +419,10 @@ impl eframe::App for EmulatorApp {
             reduce(&mut self.state, &action);
         }
     }
+
+    fn on_exit(&mut self, gl: &glow::Context) {
+        self.screen.lock().destroy(gl);
+    }
 }
 
 // When compiling natively:
@@ -269,7 +435,7 @@ fn main() {
         native_options,
         Box::new(|cc| {
             cc.egui_ctx.set_pixels_per_point(2.0);
-            Box::new(EmulatorApp::default())
+            Box::new(EmulatorApp::new(&cc))
         }),
     );
 }
